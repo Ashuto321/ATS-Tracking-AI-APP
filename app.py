f29820e015a05fdc98aa2dc2845212a1ff2921e5
Ashutosh@Pandey <112745204+Ashuto321@users.noreply.github.com>
2025-03-31 12:19:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,15 +63,13 @@ submit1= st.button("Tell me about the resume")
 
 submit2= st.button("How can i improve my skills")
 
-submit3= st.button("Percentage match")
-
 input_prompt1 = """
  You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
   Please share your professional evaluation on whether the candidate's profile aligns with the role. 
  Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 """
 
-input_prompt3 = """
+input_prompt2 = """
 You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. First the output should come as percentage and then keywords missing and last final thoughts.
@@ -89,7 +87,7 @@ if submit1:
 elif submit2:
      if uploaded_file is not None:
           pdf_content=input_pdf_setup(uploaded_file)
-          response=get_gemini_response(input_prompt3,pdf_content,input_text)          
+          response=get_gemini_response(input_prompt2,pdf_content,input_text)          
           st.subheader("The Responses is")
           st.write(response)
      else:
